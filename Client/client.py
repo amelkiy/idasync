3a346815b8dda353f3597c0ed4b60dd09f767201
urikiller <urigotman@gmail.com>
2016-12-27 18:00:29 +0200

    client not working yet. latest version.

--- Client/client.py
+++ Client/client.py
@@ -1,10 +1,13 @@
 from thread import start_new_thread
-from common.Consts import Consts
-from common.utils import recv_all, send_all
-from simple_json import json
+import threading
+from ..Common.Consts import Consts
+from ..Common.utils import recv_all_with_length, send_all_with_length
+import simplejson as json
 import socket
 import time
 import traceback
+import ssl
+import os
 
 # TODO: Sync mutex for all clients
 # TODO: Singleton client for same process
@@ -14,16 +17,35 @@ SERVER_PORT = Consts.DEFAULT_PORT
 
 class IdaSyncClient(object):
     DB_VERSIONS_FNAME = "db_versions.json"
+    s_instance = None
+    
+    @classmethod
+    def get_instance(cls, *args, **kwargs):
+        if cls.s_instance is None:
+            cls(*args, **kwargs)
+        return cls.s_instance
     
     def __init__(self, db_name, change_callback):
+        if self.s_instance is not None:
+            raise Exception("Cannot create more than one instance of IdaSyncClient")
+        else:
+            self.s_instance = self
+            
         self._change_callback = change_callback
         self._sock = None
         self._version = None
-        self.start_new_thread(self._input_thread)
+        self._cached_msg_ids = {}
+        
+        self._load_version()
+        self._lock = threading.RLock()
+        start_new_thread(self._input_thread, ())
         
     def _load_version(self):
-        d = json.load(open(self.DB_VERSIONS_FNAME, "r"))
-        return d[self.db_name]
+        if not os.path.isfile(self.DB_VERSIONS_FNAME):
+            self._version = 0
+        else:
+            d = json.load(open(self.DB_VERSIONS_FNAME, "r"))
+            self._version = d[self.db_name]
             
     def _save_version(self):
         d = json.load(open(self.DB_VERSIONS_FNAME, "r"))
@@ -31,45 +53,78 @@ class IdaSyncClient(object):
         json.dump(open(self.DB_VERSIONS_FNAME, "w"))
         
     def _send(self, data):
-        send_all(self._sock, struct.pack("<I", len(data)))
-        send_all(data)
+        if isinstance(data, dict):
+            data = json.dumps(data)
+        send_all_with_length(self._sock, data)
+        
+    def _disconnect_server(self):
+        self._lock.acquire()
+        try:
+            if self._sock is None:
+                return
+            self._sock.close()
+        except:
+            pass
+        finally:
+            self._sock = None
+            self._lock.release()
     
     def _connect_server(self):
-        if self._sock is not None:
-            return
-        sock = socket.socket()
-        sock.connect((SERVER_ADDR, SERVER_PORT))
-        send_all()
+        self._lock.acquire()
+        try:
+            if self._sock is not None:
+                return
+            sock = socket.socket()
+            sock.settimeout(10.0)
+            sock.connect((SERVER_ADDR, SERVER_PORT))
+            sock.settimeout(None)
+            sock = ssl.wrap_socket(sock)
+            self._sock = sock
+            self._send({"db_name": self._db_name, "version": self._version})
+        except:
+            self._disconnect_server()
+            traceback.print_exc()
+        finally:
+            self._lock.release()
             
     def _input_thread(self):
         while True:
-            if not sock._sock:
-                time.sleep(0.1)
+            self._connect_server()
+            if not self._sock:
+                time.sleep(1.0)
                 continue
                 
             try:
-                l = struct.unpack("<I", self._sock.recv(4))[0]
-                if l > 1024 * 1024:
-                    raise Exception("Too long input length: %d" % l)
-                data = self._sock.recv(l)
-                data = json.loads(data)
-                try:
-                    self._change_callback(data)
-                except:
-                    pass
+                data = recv_all_with_length(self._sock)
+                msg = json.loads(data)
+                msg_id = msg["id"]
+                data = msg["data"]
             except:
+                self._disconnect_server()
                 traceback.print_exc()
                 time.sleep(0.1)
+                continue
+            print "Got update message: %s" % str(data)
+            if msg_id in self._cached_msg_ids:
+                print "\tSkipping"
+                continue
+            self._change_callback(data)
+            self._cached_msg_ids[msg_id] = None
+            self._version += 1
+            self._save_version()
+            print "\tNew version: %d"
+                
         
     def submit_change(self, data):
-        if not self._sock:
-            self._connect()
+        # Only the input thread calls connect_server()
+        msg_id = os.urandom(16)
+        msg = {"id": msg_id.encode("hex"), "data": data}
             
         try:
-            self._send(data)
+            self._send(msg)
+            self._cached_msg_ids[msg_id] = None
         except:
-            self._sock.close()
-            self._sock = None
-            self._connect()
+            self._disconnect_server()
+            time.sleep(2.0)
             self._send(data)
             
